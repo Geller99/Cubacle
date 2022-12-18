489bb9f0011f3283ea97b134b20ccd7ed0fcc501
geller99 <gellera800@gmail.com>
2022-12-18 01:01:28 -0500

    Fix: User Notis

--- components/header/mobileHeader.js
+++ components/header/mobileHeader.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import mobileNavStyles from '../../styles/header.module.scss';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import Image from "next/image";
+import mobileNavStyles from "../../styles/header.module.scss";
+import { useRouter } from "next/router";
+import Profile from "../profile/profile";
 
 const MobileHeader = () => {
   const router = useRouter();
@@ -21,82 +22,121 @@ const MobileHeader = () => {
         <div className={mobileNavStyles.leftCta}>
           <span className={mobileNavStyles.hamburger} onClick={() => toggle(i)}>
             {selected === i ? (
-              <Image src={'/Images/closeIcon.svg'} height={28} width={32} alt={''} />
+              <Image
+                src={"/Images/closeIcon.svg"}
+                height={28}
+                width={32}
+                alt={""}
+              />
             ) : (
-              <Image src={'/Images/hamburgerIcon.svg'} height={28} width={32} alt={''} />
+              <Image
+                src={"/Images/hamburgerIcon.svg"}
+                height={28}
+                width={32}
+                alt={""}
+              />
             )}
           </span>
 
           <span
             className={mobileNavStyles.settingIcon}
-            onClick={() => router.push('/')}
+            onClick={() => router.push("/")}
           >
-            <Image src={'/Images/homeIcon.svg'} height={28} width={32} alt={''} />
+            <Image
+              src={"/Images/homeIcon.svg"}
+              height={28}
+              width={32}
+              alt={""}
+            />
           </span>
         </div>
 
         <div className={mobileNavStyles.rightCta}>
           <span className={mobileNavStyles.cta}>
-            <Image src={'/Images/helpIcon.svg'} height={20} width={20}  alt={''}/>
+            <Image
+              src={"/Images/helpIcon.svg"}
+              height={20}
+              width={20}
+              alt={""}
+            />
           </span>
 
           <span className={mobileNavStyles.cta}>
             <Image
-              src={'/Images/notificationIcon.png'}
+              src={"/Images/notificationIcon.png"}
               height={18}
               width={15}
-              alt={''}
+              alt={""}
             />
           </span>
 
           <div className={mobileNavStyles.userImg}>
-            <Image src={'/Images/userImg.jpg'} height={32} width={32}  alt={''}/>
+            <Image
+              src={"/Images/userImg.jpg"}
+              height={32}
+              width={32}
+              alt={""}
+            />
           </div>
         </div>
       </main>
 
       <div
         className={
-          selected === i ? 'hamburgerMenu  hamburgerMenuShows' : 'hamburgerMenu'
+          selected === i ? "hamburgerMenu  hamburgerMenuShows" : "hamburgerMenu"
         }
       >
         <ul>
           <li
             // className={navStyle.activeLink}
-            onClick={() => router.push('/assets')}
+            onClick={() => router.push("/assets")}
           >
-            <Image src={'/Images/1Icon.png'} height={32} width={32}  alt={''}/>
+            <Image src={"/Images/1Icon.png"} height={32} width={32} alt={""} />
           </li>
 
-          <li onClick={() => router.push('/nft')}>
-            <Image src={'/Images/2Icon.png'} height={32} width={32} alt={''}/>
+          <li onClick={() => router.push("/nft")}>
+            <Image src={"/Images/2Icon.png"} height={32} width={32} alt={""} />
           </li>
 
-          <li onClick={() => router.push('/rewards/rewards')}>
-            <Image src={'/Images/3Icon.png'} height={32} width={24} alt={''} />
+          <li onClick={() => router.push("/rewards/rewards")}>
+            <Image src={"/Images/3Icon.png"} height={32} width={24} alt={""} />
           </li>
 
-          <li onClick={() => router.push('/rewards/claim')}>
-            <Image src={'/Images/4Icon.png'} height={32} width={32} alt={''}/>
+          <li onClick={() => router.push("/rewards/claim")}>
+            <Image src={"/Images/4Icon.png"} height={32} width={32} alt={""} />
           </li>
 
-          <li onClick={() => router.push('/')}>
-            <Image src={'/Images/5Icon.png'} height={35} width={35} alt={''}/>
+          <li onClick={() => router.push("/")}>
+            <Image src={"/Images/5Icon.png"} height={35} width={35} alt={""} />
           </li>
         </ul>
       </div>
 
       <div className={mobileNavStyles.mobileLogo}>
-        <Image src={'/Images/mobile-logo.png'} height={58} width={149} alt={''} />
+        <Image
+          src={"/Images/mobile-logo.png"}
+          height={36}
+          width={100}
+          alt={""}
+        />
       </div>
 
-      <div className={mobileNavStyles.headerText}>
-        <h3>Dashboard</h3>
-        <span>
-          <p>Welcome to Cubex</p>
+      <div className={mobileNavStyles.headerCtaContainer}>
+        <div className={mobileNavStyles.headerText}>
+          <h3>Dashboard</h3>
+          <span>
+            <p>Welcome to Cubex</p>
 
-          <Image src={'/Images/dropdownIcon.svg'} height={12} width={12} alt={''} />
-        </span>
+            <Image
+              src={"/Images/dropdownIcon.svg"}
+              height={12}
+              width={12}
+              alt={""}
+            />
+          </span>
+        </div>
+
+        <Profile />
       </div>
     </div>
   );
--- pages/rewards/claim.js
+++ pages/rewards/claim.js
@@ -1,105 +1,176 @@
-import styles from '../../styles/rewards.module.scss';
-import Image from 'next/image';
-import { usePrepareContractWrite, useContractWrite, useAccount } from 'wagmi'
-import { testabi } from '../../config/testabi';
-import { rewardsabi } from '../../config/rewardsabi';
-import { useContractRead } from 'wagmi'
-import { useEffect } from 'react';
-import { useState } from 'react';
+import styles from "../../styles/rewards.module.scss";
+import Image from "next/image";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useAccount,
+  useProvider,
+} from "wagmi";
+import { testabi } from "../../config/testabi";
+import { rewardsabi } from "../../config/rewardsabi";
+import { useContractRead } from "wagmi";
+import { useEffect } from "react";
+import { useState } from "react";
+import { ethers } from "ethers";
 
 /**
  *
  * @returns a component for claiming rewards based on staked NFTs
+ * displays amount the user is making per claim
+ * tells you what tokens have already been claimed
+ * shows total
+ *
  *
  */
 
 const ClaimRewards = () => {
   const { address } = useAccount();
+  const provider = useProvider();
   const [tokenIds, setTokenIds] = useState([]);
+  const [eligibleTokens, setEligibleTokens] = useState([]);
+  const [unique, setUnique] = useState([]);
+  const [error, setError] = useState(null);
+
+  let myContract = new ethers.Contract(
+    "0x4F4c1A5A493b91D1F79A0c82e31e5e11850D3aA2",
+    rewardsabi,
+    provider
+  );
+
+  // check if all tokens in tokenIds are claimed already
+  // if token is claimed, remove from eligible tokens array
+  // take eligible tokens.length and pass to total pay to inform user
+  // if all tokens in wallet are already claimed, user cannot claim again
+  // pop-up modal
+
+  // map over tokenIds and check if token is T/F
+  const bulkCheck = async () => {
+    const promises = tokenIds.map(token => {
+      return threeTry(() => {
+        return myContract.tokens(parseInt(token));
+      }, token);
+    });
   
+    await Promise.all(promises);
+  };
+  
+  const threeTry = (callback, token) => {
+    return new Promise(async (resolve, reject) => {
+      let lastError;
+      for(let i = 0; i < 1; ++i){
+        try{
+           const result = await callback();
+           if (result === false) {
+                console.log("Current Approved Token", token);
+                unique.push(token);
+              }
+           resolve(result);
+           return;
+        }
+        catch(err){
+          lastError = err;
+        }
+      }
+      reject(lastError);
+    });
+  };
+
+  //   address: '0x4F4c1A5A493b91D1F79A0c82e31e5e11850D3aA2',
+  //   abi: rewardsabi,
+  //   functionName: 'tokens',
+  //   args:[tokenIds.forEach((el) => parseInt(el))],
+  //   onSuccess: (data) => {
+  //     console.log("Approved Tokens!")
+  //   },
+  //   onError: (error) => {
+  //     console.log(error)
+  //   }
+  // });
+  // const { writeAsync: check } = useContractWrite(claimRewardsConfig);
 
   /**
    * @dev fetches token Info from user address
    */
 
-  const { config: tokenFetchConfig , error: tokenFetchError } = useContractRead({
-    address: '0x8661cD0C4A3fD3Dc6B31cD24B20368851749Df00',
+  const { config: tokenFetchConfig, error: tokenFetchError } = useContractRead({
+    address: "0x8661cD0C4A3fD3Dc6B31cD24B20368851749Df00",
     abi: testabi,
-    functionName: 'getOwnerTokens',
-    args:[ address && address ],
+    functionName: "getOwnerTokens",
+    args: [address && address],
     onSuccess: (data) => {
-      setTokenIds(data.map((el) => el.toString()))
-      console.log("Founder owned tokens!", data.map((el) => el.toString()));
-    }
+      setTokenIds(data.map((el) => el.toString()));
+      console.log(
+        "Founder owned tokens!",
+        data.map((el) => el.toString())
+      );
+    },
   });
   const { write: fetchTokenIds } = useContractWrite(tokenFetchConfig);
 
 
   /**
-   * @dev requests setApproval Call for token Transfer
-   */
-
-  const { config: approvalConfig , error: approvalError } = usePrepareContractWrite({
-    address: '0x8661cD0C4A3fD3Dc6B31cD24B20368851749Df00',
-    abi: testabi,
-    functionName: 'setApprovalForAll',
-    args:[ '0x1331b052f27e981aff245C5DD1a1837CB93Ba532', 'true' ],
-    onSuccess: (data) => {
-      console.log("Approved Tokens!")
-    },
-    onError: (error) => {
-      console.log(error)
-    }
-  });
-  const { writeAsync: approve } = useContractWrite(approvalConfig);
-
-  /**
    * @dev setup contract write for claimRewards
    */
 
-  const { config: claimRewardsConfig , error: claimRewardsError } = usePrepareContractWrite({
-    address: '0x4F4c1A5A493b91D1F79A0c82e31e5e11850D3aA2',
-    abi: rewardsabi,
-    functionName: 'claimRewards',
-    args:[tokenIds.map((el) => parseInt(el))],
-    onSuccess: (data) => {
-      console.log("Approved Tokens!")
-    },
-    onError: (error) => {
-      console.log(error)
-    }
-  });
+  const { config: claimRewardsConfig, error: claimRewardsError } =
+    usePrepareContractWrite({
+      address: "0x4F4c1A5A493b91D1F79A0c82e31e5e11850D3aA2",
+      abi: rewardsabi,
+      functionName: "claimRewards",
+      args: [tokenIds.map((el) => parseInt(el))],
+      onSuccess: (data) => {
+        console.log("Approved Tokens!");
+      },
+      onError: (error) => {
+        console.log(error);
+        setError("Claim Not Allowed at this time");
+      },
+    });
   const { writeAsync: claim } = useContractWrite(claimRewardsConfig);
 
-
   useEffect(() => {
-    
     fetchTokenIds?.();
     //eslint-disable-next-line
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (tokenIds.length > 0) bulkCheck();
+        //eslint-disable-next-line
+  }, [tokenIds]);
+
+  useEffect(() => {
+    console.log('Eligible', eligibleTokens)
+    setUnique([...new Set(eligibleTokens)]);
+  }, [eligibleTokens])
 
   const claimRewardsHandler = async () => {
     if (tokenIds.length < 1) {
-      alert('You do not own any Cubex Cards');
+      alert("You do not own any Cubex Cards");
       return;
     }
-      console.log("Trigger setApproval");
-      try {
-        // await approve?.();
-        await claim?.();
-        alert("Successfully Claimed!");
-      } catch (error) {
-        console.log(error)
-      }
-      
-  }
+    if (unique.length < 1) {
+      alert("All of your tokens have been claimed");
+      return;
+    }
+    let totalPay = unique.length * 11;
+    alert(`Your claim for the CubeX staking rewards is $${totalPay}`);
+    console.log("User Token List", unique);
+
+    
+    try {
+      await claim?.();
+      if (error) alert(error);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const claimRewards = [
     {
-      rewardImg: '',
-      rewardName: 'Staking Reward',
-      rewardDescription: 'Eth Drop Reward For Cubex Cards',
-    }
+      rewardImg: "",
+      rewardName: "Staking Reward",
+      rewardDescription: "Eth Drop Reward For Cubex Cards",
+    },
   ];
 
   return (
@@ -108,24 +179,33 @@ const ClaimRewards = () => {
         {claimRewards.map((claimreward, idx) => {
           return (
             <div className={styles.reward} key={claimreward.rewardName}>
-
               <div className={styles.rewardDetails}>
-                <Image src={claimreward.rewardImg} height={92} width={92} alt={''} />
+                <Image
+                  src={claimreward.rewardImg}
+                  height={92}
+                  width={92}
+                  alt={""}
+                />
                 <span>
                   <h6>{claimreward.rewardName}</h6>
                   <p>{claimreward.rewardDescription}</p>
                 </span>
               </div>
 
-              <button className={styles.amount} onClick={() => claimRewardsHandler()} >
+              <button
+                className={styles.amount}
+                onClick={() => claimRewardsHandler()}
+              >
                 <Image
                   className={styles.icon}
-                  src={'/Images/4Icon.png'}
+                  src={"/Images/4Icon.png"}
                   height={22}
                   width={22}
-                  alt={''}
+                  alt={""}
                 />
                 <p>Claim</p>
+
+              
               </button>
             </div>
           );

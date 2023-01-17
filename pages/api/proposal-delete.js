063b877cbbb5d3e9e6a9abc83d49e2c93dd23d15
geller99 <gellera800@gmail.com>
2023-01-17 02:05:50 -0500

    Fixed deployment bug

--- pages/api/proposal-delete.js
+++ pages/api/proposal-delete.js
@@ -1,28 +1,28 @@
 
-/**
- * @dev fetches proposal id, deletes from DB
- * 
- * @dev uses EIP signature to verify before api can be called
- * 
- * 
- */
+// /**
+//  * @dev fetches proposal id, deletes from DB
+//  * 
+//  * @dev uses EIP signature to verify before api can be called
+//  * 
+//  * 
+//  */
 
-export const deleteProposal = (req, res) => {
-    try {
+// export const deleteProposal = (req, res) => {
+//     try {
         
-        // grab proposal Id and delete from DB
-        // const { id } = req.body;
+//         // grab proposal Id and delete from DB
+//         // const { id } = req.body;
 
-        // A.findByIdAndRemove(id, callback) // executes
-        res.send('Proposal Deleted');
+//         // A.findByIdAndRemove(id, callback) // executes
+//         res.send('Proposal Deleted');
         
-      } catch (error) {
-        res.status(400).json({
-          status: "failed",
-          data: {
-            user: "unable to delete Proposal",
-          },
-        });
-      }
+//       } catch (error) {
+//         res.status(400).json({
+//           status: "failed",
+//           data: {
+//             user: "unable to delete Proposal",
+//           },
+//         });
+//       }
   
-}
+// }

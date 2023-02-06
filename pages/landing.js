19d0fcff368ee085fdc1f6c625b0c225185015b6
Kayphrosh <olakunbiolabode@yahoo.com>
2023-02-06 05:46:39 +0100

    new tabs

--- pages/landing.js
+++ pages/landing.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+// import { Icon } from '@iconify/react';
 import { useAccount } from 'wagmi';
 import styles from '../styles/landing.module.scss';
 import Image from 'next/image';
@@ -13,160 +14,190 @@ const Landing = () => {
     setUser(address);
   }, [address]);
 
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const handleClick = (direction) => {
+    const container = document.querySelector(`.${styles.container}`);
+    setScrollLeft(scrollLeft + direction * 200);
+    container.scrollLeft += direction * 200;
+  };
   return (
-    <section className={styles.container}>
-      <main>
-        <div
-          className={styles.nfts}
-          onClick={() =>
-            user ? router.push('/assets') : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/1Icon.png'}
-            height={80}
-            width={80}
-            alt={''}
-          />
+    <div className={styles.landingContainer}>
+      <section className={styles.container}>
+        <main className={styles.main}>
+          <div
+            className={styles.nfts}
+            onClick={() =>
+              user ? router.push('/assets') : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/1Icon.png'}
+              height={80}
+              width={80}
+              alt={''}
+            />
 
-          <span>
-            <h3>Your NFTS</h3>
-            <p>Click here to see all your Cubex NFTs and Assets...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Your NFTS</h3>
+              <p>Click here to see all your Cubex NFTs and Assets...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.roadmap}
-          onClick={() =>
-            user ? router.push('/roadmap') : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/2Icon.png'}
-            height={80}
-            width={80}
-            alt={''}
-          />
+          <div
+            className={styles.roadmap}
+            onClick={() =>
+              user ? router.push('/roadmap') : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/2Icon.png'}
+              height={80}
+              width={80}
+              alt={''}
+            />
 
-          <span>
-            <h3>Roadmap</h3>
-            <p>Keep up with the team at Cubex, checkout our roadmap!...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Roadmap</h3>
+              <p>Keep up with the team at Cubex, checkout our roadmap!...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.rewards}
-          onClick={() =>
-            user
-              ? router.push('/rewards/rewards')
-              : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/3Icon.png'}
-            height={76}
-            width={62}
-            alt={''}
-          />
+          <div
+            className={styles.rewards}
+            onClick={() =>
+              user
+                ? router.push('/rewards/rewards')
+                : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/3Icon.png'}
+              height={76}
+              width={62}
+              alt={''}
+            />
 
-          <span>
-            <h3>Rewards</h3>
-            <p>Explore upcoming rewards for the community...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Rewards</h3>
+              <p>Explore upcoming rewards for the community...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.claimRewards}
-          onClick={() =>
-            user
-              ? router.push('/rewards/claim')
-              : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/4Icon.png'}
-            height={70}
-            width={70}
-            alt={''}
-          />
+          <div
+            className={styles.claimRewards}
+            onClick={() =>
+              user
+                ? router.push('/rewards/claim')
+                : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/4Icon.png'}
+              height={70}
+              width={70}
+              alt={''}
+            />
 
-          <span>
-            <h3>Claim</h3>
-            <p>Explore and Claim Your CubeX Rewards Here!...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Claim</h3>
+              <p>Explore and Claim Your CubeX Rewards Here!...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.shop}
-          onClick={() =>
-            user ? router.push('/') : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/5Icon.png'}
-            height={80}
-            width={80}
-            alt={''}
-          />
+          <div
+            className={styles.shop}
+            onClick={() =>
+              user ? router.push('/') : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/5Icon.png'}
+              height={80}
+              width={80}
+              alt={''}
+            />
 
-          <span>
-            <h3>Shop</h3>
-            <p>The Official CubeX Shop for Holders, Coming Soon...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Shop</h3>
+              <p>The Official CubeX Shop for Holders, Coming Soon...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.nfts}
-          onClick={() =>
-            user ? router.push('/assets') : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/1Icon.png'}
-            height={80}
-            width={80}
-            alt={''}
-          />
+          <div
+            className={styles.nfts}
+            onClick={() =>
+              user ? router.push('/assets') : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/1Icon.png'}
+              height={80}
+              width={80}
+              alt={''}
+            />
 
-          <span>
-            <h3>Vote</h3>
-            <p>Click here to see all your Cubex NFTs and Assets...</p>
-          </span>
-        </div>
+            <span>
+              <h3>Vote</h3>
+              <p>Click here to see all your Cubex NFTs and Assets...</p>
+            </span>
+          </div>
 
-        <div
-          className={styles.nfts}
-          onClick={() =>
-            user ? router.push('/assets') : alert('Please connect wallet')
-          }
-        >
-          <Image
-            className={styles.icon}
-            src={'/Images/1Icon.png'}
-            height={80}
-            width={80}
-            alt={''}
-          />
+          <div
+            className={styles.nfts}
+            onClick={() =>
+              user ? router.push('/assets') : alert('Please connect wallet')
+            }
+          >
+            <Image
+              className={styles.icon}
+              src={'/Images/1Icon.png'}
+              height={80}
+              width={80}
+              alt={''}
+            />
 
-          <span>
-            <h3>Admim</h3>
-            <p>Click here to see all your Cubex NFTs and Assets...</p>
-          </span>
-        </div>
-      </main>
+            <span>
+              <h3>Admim</h3>
+              <p>Click here to see all your Cubex NFTs and Assets...</p>
+            </span>
+          </div>
+        </main>
 
-      <span className={styles.scrollIcon}>
+        {/* <span className={styles.scrollIcon}>
         <span className={styles.scrollIconWheelOuter}>
           <span className={styles.scrollIconWheelInner}></span>
         </span>
+      </span> */}
+      </section>
+      <span className={styles.scrollBtn}>
+        <button onClick={() => handleClick(-2)}>
+          <Image
+            className={styles.icon}
+            src={'/Images/leftArrow.svg'}
+            height={24}
+            width={24}
+            alt={''}
+          />
+        </button>
+
+        <button onClick={() => handleClick(2)}>
+        <Image
+            className={styles.icon}
+            src={'/Images/rightArrow.svg'}
+            height={24}
+            width={24}
+            alt={''}
+          />
+        </button>
       </span>
-    </section>
+    </div>
   );
 };
 
